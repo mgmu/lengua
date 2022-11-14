3a96a7e2bbb64388f13da34516fe12bf0159b43e
MORON USON guillermo <guillermo.moron@laposte.net>
2022-11-14 15:46:45 +0100

    added dictionary selection in adapter

--- app/src/main/java/fr/uparis/lengua/TranslationResearchFragment.kt
+++ app/src/main/java/fr/uparis/lengua/TranslationResearchFragment.kt
@@ -1,11 +1,41 @@
 package fr.uparis.lengua
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import fr.uparis.lengua.databinding.FragmentTranslationResearchBinding
 
-class TranslationResearchFragment: Fragment(R.layout.fragment_translation_research) {
+class TranslationResearchFragment(private val adapter: DictionaryRecyclerAdapter):
+    Fragment(R.layout.fragment_translation_research) {
+
+    private lateinit var binding: FragmentTranslationResearchBinding
+    private val model: TranslationViewModel by activityViewModels()
 
     companion object {
         @JvmStatic
-        fun newInstance() = TranslationResearchFragment()
+        fun newInstance(adapter: DictionaryRecyclerAdapter) = TranslationResearchFragment(adapter)
+    }
+
+    override fun onStart() {
+
+        /* Observe changes in dictionaries of database */
+        model.allDictionaries.observe(this) {
+            adapter.dictionaries = it
+            adapter.notifyDataSetChanged()
+        }
+
+        /* load all dictionaries */
+        model.loadAllDictionaries()
+
+        super.onStart()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentTranslationResearchBinding.bind(view)
+        binding.dictionaryRecycler.layoutManager = LinearLayoutManager(context)
+        binding.dictionaryRecycler.adapter = adapter
     }
 }
---
+++ app/src/main/java/fr/uparis/lengua/TranslationViewModel.kt
@@ -0,0 +1,24 @@
+package fr.uparis.lengua
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class TranslationViewModel(app: Application): AndroidViewModel(app) {
+
+    /* DB Request interface */
+    private val dao = (app as TranslationApplication).database.iDao()
+
+    /* All dictionaries of database */
+    var allDictionaries: LiveData<List<Dictionary>> = loadAllDictionaries()
+
+    /* Selected dictionary in research page */
+    var selectedDictionary = MutableLiveData<Dictionary>()
+
+    /* Loads all dictionaries in database */
+    fun loadAllDictionaries() = dao.loadAllDictionaries()
+
+    /* Loads all words in database */
+    fun loadAllWords() = dao.loadAllWords()
+}
