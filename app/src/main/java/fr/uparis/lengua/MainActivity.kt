61d2eb98358bb6168f38719fd4632143997c815d
MORON USON guillermo <guillermo.moron@laposte.net>
2022-11-14 12:51:36 +0100

    updated compileSdkversion to 33 (previously 32) added viewBinding for main activity and updated its gui xml

--- app/src/main/java/fr/uparis/lengua/MainActivity.kt
+++ app/src/main/java/fr/uparis/lengua/MainActivity.kt
@@ -2,10 +2,15 @@ package fr.uparis.lengua
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.uparis.lengua.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
     }
 }
