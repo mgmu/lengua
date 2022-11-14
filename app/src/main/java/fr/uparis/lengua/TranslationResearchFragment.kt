c0a84578acf7a491c3b898b9630791e6c3ee7ac8
MORON USON guillermo <guillermo.moron@laposte.net>
2022-11-14 12:40:05 +0100

    added fragment for translation research (blank fragment)

---
+++ app/src/main/java/fr/uparis/lengua/TranslationResearchFragment.kt
@@ -0,0 +1,11 @@
+package fr.uparis.lengua
+
+import androidx.fragment.app.Fragment
+
+class TranslationResearchFragment: Fragment(R.layout.fragment_translation_research) {
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = TranslationResearchFragment()
+    }
+}
