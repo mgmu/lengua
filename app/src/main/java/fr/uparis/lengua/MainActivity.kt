319eb115fe6e16162b9d3e05a13576936a6935ef
MORON USON guillermo <guillermo.moron@laposte.net>
2022-11-14 13:05:22 +0100

    added pager implementation

--- app/src/main/java/fr/uparis/lengua/MainActivity.kt
+++ app/src/main/java/fr/uparis/lengua/MainActivity.kt
@@ -2,6 +2,10 @@ package fr.uparis.lengua
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import fr.uparis.lengua.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +16,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        /* Page names */
+        val names = listOf("Research")
+
+        /* Fragments creation */
+        val translationResearchFragment = TranslationResearchFragment.newInstance()
+
+        /* Pager Adapter */
+        val pagerAdapter = ScreenSlidePagerAdapter(
+            this,
+            mutableListOf(translationResearchFragment)
+        )
+
+        /* Attach pager adapter to pager */
+        binding.pager.adapter = pagerAdapter
+
+        /* Attach page name to tab */
+        TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
+            tab.text = names[position]
+        }.attach()
+    }
+
+    /* Associates a fragment to a page of the ViewPager */
+    class ScreenSlidePagerAdapter(fa: FragmentActivity,
+                                  var fragments: MutableList<Fragment>): FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = fragments.size
+        override fun createFragment(position: Int): Fragment = fragments[position]
     }
 }
