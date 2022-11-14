c7087bd2e779413a7e336b3a6eb34a1d56e8ba42
MORON USON guillermo <guillermo.moron@laposte.net>
2022-11-14 14:32:59 +0100

    added layout of item layout

---
+++ app/src/main/java/fr/uparis/lengua/DictionaryRecyclerAdapter.kt
@@ -0,0 +1,22 @@
+package fr.uparis.lengua
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import fr.uparis.lengua.databinding.ItemLayoutBinding
+
+class DictionaryRecyclerAdapter: RecyclerView.Adapter<DictionaryRecyclerAdapter.VH>() {
+
+    class VH(val binding: ItemLayoutBinding): RecyclerView.ViewHolder(binding.root) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
