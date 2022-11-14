2c0bf664ec87cc090c027badb5454ec78d50fc95
MORON USON guillermo <guillermo.moron@laposte.net>
2022-11-14 15:05:48 +0100

    defined entites created database

--- app/src/main/java/fr/uparis/lengua/TranslationDatabase.kt
+++ app/src/main/java/fr/uparis/lengua/TranslationDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+
 @Database(entities = [Dictionary::class, Word::class], version = 4)
 abstract class TranslationDatabase: RoomDatabase() {
     abstract fun iDao(): IDao
