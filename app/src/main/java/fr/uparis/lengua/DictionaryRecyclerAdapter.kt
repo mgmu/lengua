ff10608b13ea1c5386bee6194fb5356783617caf
MORON USON guillermo <guillermo.moron@laposte.net>
2022-11-14 15:46:45 +0100

    added dictionary selection in adapter

--- app/src/main/java/fr/uparis/lengua/DictionaryRecyclerAdapter.kt
+++ app/src/main/java/fr/uparis/lengua/DictionaryRecyclerAdapter.kt
@@ -1,22 +1,73 @@
 package fr.uparis.lengua
 
+import android.graphics.Color
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import fr.uparis.lengua.databinding.ItemLayoutBinding
 
-class DictionaryRecyclerAdapter: RecyclerView.Adapter<DictionaryRecyclerAdapter.VH>() {
+class DictionaryRecyclerAdapter(private val model: TranslationViewModel):
+    RecyclerView.Adapter<DictionaryRecyclerAdapter.VH>() {
 
-    class VH(val binding: ItemLayoutBinding): RecyclerView.ViewHolder(binding.root) {}
+    /* List of dictionaries to display */
+    var dictionaries: List<Dictionary> = listOf()
+
+    class VH(val binding: ItemLayoutBinding): RecyclerView.ViewHolder(binding.root) {
+        var dictionary: Dictionary? = null
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
-        TODO("Not yet implemented")
+
+        /* Binding for dictionary item */
+        val itemBinding = ItemLayoutBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        val holder = VH(itemBinding)
+
+        /* Selection listener */
+        holder.binding.root.setOnClickListener {
+            when (model.selectedDictionary.value) {
+                null -> { /* No selected dictionary */
+                    model.selectedDictionary.value = holder.dictionary
+                    notifyItemChanged(dictionaries.indexOf(holder.dictionary))
+                }
+
+                holder.dictionary -> { /* Unselect dictionary */
+                    model.selectedDictionary.value = null
+                    notifyItemChanged(dictionaries.indexOf(holder.dictionary))
+                }
+
+                else -> { /* Change selected dictionary */
+                    val previous = model.selectedDictionary.value
+                    model.selectedDictionary.value = holder.dictionary
+                    notifyItemChanged(dictionaries.indexOf(holder.dictionary))
+                    notifyItemChanged(dictionaries.indexOf(previous))
+                }
+            }
+        }
+
+        return holder
     }
 
     override fun onBindViewHolder(holder: VH, position: Int) {
-        TODO("Not yet implemented")
+        /* assign corresponding dictionary */
+        holder.dictionary = dictionaries[position]
+
+        /* assign values to corresponding fields and item background color */
+        with (holder.binding) {
+            dictionaryNameTextView.text = holder.dictionary!!.name
+            dictionaryLinkTextView.text = holder.dictionary!!.link
+
+            val color =
+                if (holder.dictionary == model.selectedDictionary.value)
+                    Color.RED   /* selected color */
+                else if (position % 2 == 0)
+                    Color.BLUE  /* even color */
+                else
+                    Color.GREEN /* odd color */
+
+            holder.itemView.setBackgroundColor(color)
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = dictionaries.size
 }
