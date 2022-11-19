0da9520f1e328356c82c0d53c11ad02854b67764
Guillermo Moron Uson <guillermo.moron-uson@etu.u-paris.fr>
2022-11-19 19:57:20 +0100

    started implementation of save button action in SaveSearchFragment

--- app/src/main/java/fr/uparis/lengua/SaveSearchFragment.kt
+++ app/src/main/java/fr/uparis/lengua/SaveSearchFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import fr.uparis.lengua.databinding.FragmentSaveSearchBinding
 
 class SaveSearchFragment : Fragment(R.layout.fragment_save_search) {
+
+    private lateinit var binding: FragmentSaveSearchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,6 +17,31 @@ class SaveSearchFragment : Fragment(R.layout.fragment_save_search) {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_save_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSaveSearchBinding.bind(view)
+
+        /* On Save button click, add word to DB */
+        binding.saveSearchButton.setOnClickListener {
+            val word = binding.wordToSaveEditText.text.toString()
+            val src = binding.sourceLanguageEditText.text.toString()
+            val dest = binding.destinationLanguageEditText.text.toString()
+            val uri = binding.dictionaryUriEditText.text.toString()
+            val dict = binding.dictionaryNameEditText.text.toString()
+
+            /* Do nothing if fields are missing */
+            if (word.isBlank()
+                || src.isBlank()
+                || dest.isBlank()
+                || uri.isBlank()
+                || dict.isBlank()) {
+                return@setOnClickListener
+            }
+
+            TODO()
+        }
     }
 
     companion object {
