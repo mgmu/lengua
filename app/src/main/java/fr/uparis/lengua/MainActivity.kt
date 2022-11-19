eadaa65d3ad4ff7e0a3c84a2858d132ab4bbd3ab
Guillermo Moron Uson <guillermo.moron-uson@etu.u-paris.fr>
2022-11-19 11:49:26 +0100

    fixed service not starting the function fillWords causes a NullPointerException

--- app/src/main/java/fr/uparis/lengua/MainActivity.kt
+++ app/src/main/java/fr/uparis/lengua/MainActivity.kt
@@ -1,14 +1,11 @@
 package fr.uparis.lengua
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 import fr.uparis.lengua.databinding.ActivityMainBinding
@@ -44,10 +41,12 @@ class MainActivity : AppCompatActivity() {
         TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
             tab.text = names[position]
         }.attach()
-        val i = Intent(this,LearningService::class.java)
-        Log.d("service","before startService in main")
+
+        /* Launch Notification Service */
+        val i = Intent(this, LearningService::class.java)
+        Log.d("logLENGUA","before startService in main")
         applicationContext.startService(i)
-        Log.d("service","after startService in main")
+        Log.d("logLENGUA","after startService in main")
 
 
     }
