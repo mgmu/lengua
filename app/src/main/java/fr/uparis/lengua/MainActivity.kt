8f328df1a26dc99e569cf79c082fc121f4bb76d5
MORON USON guillermo <guillermo.moron@laposte.net>
2022-11-30 17:02:40 +0100

    defined entites created database

--- app/src/main/java/fr/uparis/lengua/MainActivity.kt
+++ app/src/main/java/fr/uparis/lengua/MainActivity.kt
@@ -3,6 +3,7 @@ package fr.uparis.lengua
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import android.util.Log
 import androidx.fragment.app.Fragment
