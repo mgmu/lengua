11c9f318193cd5579ac4552c9317d2306ff86311
DERVISHI <sevidervishi@outlook.fr>
2022-12-10 12:42:16 +0100

    service sends notifications with database words.

--- app/src/main/java/fr/uparis/lengua/LearningService.kt
+++ app/src/main/java/fr/uparis/lengua/LearningService.kt
@@ -1,12 +1,7 @@
 package fr.uparis.lengua
 
 import android.app.*
-<<<<<<< HEAD
-import android.content.Intent
-import android.content.IntentFilter
 import android.content.BroadcastReceiver
-=======
->>>>>>> e0cf0df (rebased main)
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.IBinder
@@ -17,7 +12,6 @@ import android.content.Context
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import android.net.Uri
-<<<<<<< HEAD
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.LiveData
 import android.widget.Toast
@@ -25,22 +19,10 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.viewmodel.viewModelFactory
 import android.os.SystemClock
 
-
-
-=======
->>>>>>> e0cf0df (rebased main)
-
 /**
  * This service allows the user to review words by using notifications.
  */
 class LearningService : LifecycleService() {
-
-
-    private val REMAINING_NOTIFICATIONS = "number of notifications to send for next course"
-    private val HOUR_ = "hour"
-    private val MINUTE_ = "minute"
-    private val RECAP_FREQUENCY_ = "repeatingInterval"
-
 
     private val REMAINING_NOTIFICATIONS = "number of notifications to send for next course"
     private val HOUR_ = "hour"
