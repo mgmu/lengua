29dddb53bb1e8661388bcc874039ab5492234a54
MORON USON guillermo <guillermo.moron@laposte.net>
2022-12-21 11:59:17 +0100

    Implemented display of notifications

--- app/src/main/java/fr/uparis/lengua/LearningService2.kt
+++ app/src/main/java/fr/uparis/lengua/LearningService2.kt
@@ -3,10 +3,14 @@ package fr.uparis.lengua
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.LiveData
 
@@ -18,6 +22,13 @@ class LearningService2 : LifecycleService() { /* for observers */
     private val dao by lazy {(application as TranslationApplication).database.iDao()}
     private lateinit var allWordsInDB: LiveData<List<Word>> /* All the words in the DB. */
     private var notificationsDisplayed = false /* Indicates if the notification has been displayed. */
+    private val CHANNEL_ID = "Lengua Notification Channel ID"
+    private val CHANNEL_NAME = "Lengua Notification Channel"
+    private val CHANNEL_DESCRIPTION = "Lengua Notification Channel Description"
+
+    private val notificationManager by lazy {
+        getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+    }
 
     private val alarmManager by lazy {
         getSystemService(Context.ALARM_SERVICE) as AlarmManager
@@ -33,6 +44,21 @@ class LearningService2 : LifecycleService() { /* for observers */
         super.onCreate()
         Log.d(TAG, "onCreate()")
         allWordsInDB = dao.loadAllWords()
+        createNotificationChannel()
+    }
+
+    /**
+     * Creates the notification channel for this App
+     * */
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME, // Nom du channel
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply { description = CHANNEL_DESCRIPTION }
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -55,10 +81,32 @@ class LearningService2 : LifecycleService() { /* for observers */
         return START_NOT_STICKY
     }
 
+    /**
+     * Draws at most notificationsToDisplay random different words from the loaded words
+     * and creates a notification for each of them which is finally notified to the notification
+     * manager.
+     * */
     private fun displayNotifications() {
-        notificationsDisplayed = true
         Log.d(TAG, "displayNotifications()")
-        Log.d(TAG, "words to notify: ${drawRandomWords()}")
+        notificationsDisplayed = true
+        val words = drawRandomWords() // Draw at most notificationsToDisplay words from loaded words
+        val notifications = mutableListOf<Notification>() // Will hold the notifications with words
+        for (word in words)
+            notifications.add(createNotificationFromWord(word))
+        for (id in 0 until notifications.size) {
+            notificationManager.notify(id, notifications[id]) // Display each notification
+        }
+    }
+
+    /**
+     * Creates a Notification from the word and returns it.
+     * */
+    private fun createNotificationFromWord(word: Word): Notification {
+        return NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(word.word)
+            .setContentText("${word.sourceLanguage} -> ${word.destinationLanguage}")
+            .setSmallIcon(R.drawable.github)
+            .build()
     }
 
     override fun onBind(intent: Intent): IBinder {
@@ -72,7 +120,10 @@ class LearningService2 : LifecycleService() { /* for observers */
         super.onDestroy()
     }
 
-    /* Returns a list containing at most notificationsToDisplay words. */
+    /**
+     * Draws notificationsToDisplay different random words from the loaded words and returns
+     * them in a list. If there are no loaded words, returns an empty list.
+     * */
     private fun drawRandomWords(): List<Word> {
         if (allWordsInDB.value != null) { // If words have not been retrieved yet, do nothing
 
